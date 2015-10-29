321c16fbb1dc9ab5fb35e2bb0f733e315d005706
Olay Gureje <ogureje@yahoo.com>
2015-10-29 18:37:38 -0400

    First Screen

--- app/src/main/java/com/example/ogure/keanunitrolley/Model/utilities/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.ogure.keanunitrolley.Model.utilities;
-
-/**
- * Created by ogure on 10/1/2015.
- */
-public class Constant {
-    public static final String BASE_URL = "http://keantrolley.bbapi.co/stations";
-}
